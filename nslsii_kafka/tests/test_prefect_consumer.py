2c7c102160c455548627d3312b6fdf2e598bede2
Padraic Shafer <76011594+padraic-shafer@users.noreply.github.com>
2024-02-28 11:33:02 -0800

    test_arg_parser uses provided args

--- nslsii_kafka/tests/test_prefect_consumer.py
+++ nslsii_kafka/tests/test_prefect_consumer.py
@@ -5,7 +5,7 @@ def test_arg_parser():
     ""
     args = ["arg_0", "arg_1"]
     arg_parser = get_arg_parser()
-    parsed_args = arg_parser.parse_args()
+    parsed_args = arg_parser.parse_args(args)
 
     assert parsed_args.endstation == "arg_0"
     assert parsed_args.deployment_name == "arg_1"
