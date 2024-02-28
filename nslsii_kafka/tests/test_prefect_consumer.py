021d369d2387db725e2826de8f7998ad51b1a2f1
Padraic Shafer <76011594+padraic-shafer@users.noreply.github.com>
2024-02-28 11:35:23 -0800

    Fix invalid variable name in test_arg_parser

--- nslsii_kafka/tests/test_prefect_consumer.py
+++ nslsii_kafka/tests/test_prefect_consumer.py
@@ -2,11 +2,11 @@ from ..consumer.prefect_consumer import get_arg_parser
 
 
 def test_arg_parser():
-    ""
+    "Test that CLI arguments are assigned as expected"
     args = ["arg_0", "arg_1"]
     arg_parser = get_arg_parser()
     parsed_args = arg_parser.parse_args(args)
 
     assert parsed_args.endstation == "arg_0"
     assert parsed_args.deployment_name == "arg_1"
-    assert parsed_args.kafka-config-file == "/etc/bluesky/kafka.yml"
+    assert parsed_args.kafka_config_file == "/etc/bluesky/kafka.yml"
