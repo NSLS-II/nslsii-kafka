0bd357a89b46b11bac0521878ffd79ed85979e38
Padraic Shafer <76011594+padraic-shafer@users.noreply.github.com>
2024-02-28 11:31:14 -0800

    Initialize tests with test_prefect_consumer

---
+++ nslsii_kafka/tests/test_prefect_consumer.py
@@ -0,0 +1,12 @@
+from ..consumer.prefect_consumer import get_arg_parser
+
+
+def test_arg_parser():
+    ""
+    args = ["arg_0", "arg_1"]
+    arg_parser = get_arg_parser()
+    parsed_args = arg_parser.parse_args()
+
+    assert parsed_args.endstation == "arg_0"
+    assert parsed_args.deployment_name == "arg_1"
+    assert parsed_args.kafka-config-file == "/etc/bluesky/kafka.yml"
